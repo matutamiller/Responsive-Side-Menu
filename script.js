7f3362c879418fe1f5a010b08346a603c7a0c1e2
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-02-09 15:16:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){                                             //open slide menu
+      document.getElementById('side-menu').style.width = '250px';
+      document.getElementById('main').style.marginLeft = '250px';
+    }
+
+    function closeSlideMenu(){                                         //close slide menu
+      document.getElementById('side-menu').style.width = '0';
+      document.getElementById('main').style.marginLeft = '0';
+    }
